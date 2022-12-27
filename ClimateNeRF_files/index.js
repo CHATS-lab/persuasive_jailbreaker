9eaaf9277bd9aa30b59448715780e19f220e53c2
ClimateNeRF <118839729+ClimateNeRF@users.noreply.github.com>
2022-12-27 18:16:52 +0800

    upd

--- ClimateNeRF_files/index.js
+++ ClimateNeRF_files/index.js
@@ -1,23 +1,23 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "https://homes.cs.washington.edu/~kpar/nerfies/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+// var INTERP_BASE = "https://homes.cs.washington.edu/~kpar/nerfies/interpolation/stacked";
+// var NUM_INTERP_FRAMES = 240;
 
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
+// var interp_images = [];
+// function preloadInterpolationImages() {
+//   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+//     var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+//     interp_images[i] = new Image();
+//     interp_images[i].src = path;
+//   }
+// }
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
+// function setInterpolationImage(i) {
+//   var image = interp_images[i];
+//   image.ondragstart = function() { return false; };
+//   image.oncontextmenu = function() { return false; };
+//   $('#interpolation-image-wrapper').empty().append(image);
+// }
 
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
@@ -67,13 +67,13 @@ $(document).ready(function() {
     	});
     }
 
-    preloadInterpolationImages();
+    // preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
     bulmaSlider.attach();
 })
@@ -95,41 +95,41 @@ $(window).on("load", function(){
     });
 
 
-    const position = { x: 0, y: 0 }
-    const box = $('.hyper-space');
-    const cursor = $('.hyper-space-cursor');
-    interact('.hyper-space-cursor').draggable({
-      listeners: {
-        start (event) {
-          console.log(event.type, event.target)
-        },
-        move (event) {
-          position.x += event.dx
-          position.y += event.dy
+    // const position = { x: 0, y: 0 }
+    // const box = $('.hyper-space');
+    // const cursor = $('.hyper-space-cursor');
+    // interact('.hyper-space-cursor').draggable({
+    //   listeners: {
+    //     start (event) {
+    //       console.log(event.type, event.target)
+    //     },
+    //     move (event) {
+    //       position.x += event.dx
+    //       position.y += event.dy
 
-          event.target.style.transform =
-            `translate(${position.x}px, ${position.y}px)`
+    //       event.target.style.transform =
+    //         `translate(${position.x}px, ${position.y}px)`
 
-          let childPos = cursor.offset();
-          let parentPos = box.offset();
-          let childSize = cursor.outerWidth();
-          let point = {
-              x: (childPos.left - parentPos.left),
-              y: (childPos.top - parentPos.top)
-          };
-          point = {
-            x: (point.x) / (box.innerWidth() - childSize),
-            y: (point.y) / (box.innerHeight() - childSize)
-          }
-          updateHyperGrid(point);
-        },
-      },
-      modifiers: [
-        interact.modifiers.restrictRect({
-          restriction: 'parent'
-        })
-      ]
-    });
+    //       let childPos = cursor.offset();
+    //       let parentPos = box.offset();
+    //       let childSize = cursor.outerWidth();
+    //       let point = {
+    //           x: (childPos.left - parentPos.left),
+    //           y: (childPos.top - parentPos.top)
+    //       };
+    //       point = {
+    //         x: (point.x) / (box.innerWidth() - childSize),
+    //         y: (point.y) / (box.innerHeight() - childSize)
+    //       }
+    //       updateHyperGrid(point);
+    //     },
+    //   },
+    //   modifiers: [
+    //     interact.modifiers.restrictRect({
+    //       restriction: 'parent'
+    //     })
+    //   ]
+    // });
 
 });
 
@@ -138,10 +138,10 @@ Number.prototype.clamp = function(min, max) {
 };
 
 
-function updateHyperGrid(point) {
-  const n = 20 - 1;
-  let top = Math.round(n * point.y.clamp(0, 1)) * 100;
-  let left = Math.round(n * point.x.clamp(0, 1)) * 100;
-  $('.hyper-grid-rgb > img').css('left', -left + '%');
-  $('.hyper-grid-rgb > img').css('top', -top + '%');
-}
+// function updateHyperGrid(point) {
+//   const n = 20 - 1;
+//   let top = Math.round(n * point.y.clamp(0, 1)) * 100;
+//   let left = Math.round(n * point.x.clamp(0, 1)) * 100;
+//   $('.hyper-grid-rgb > img').css('left', -left + '%');
+//   $('.hyper-grid-rgb > img').css('top', -top + '%');
+// }
